ff4a2dc431aa4562643cfa531133c901be48daae
Tracy Ngo <tngo1206@gmail.com>
2019-02-15 03:51:33 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -19,10 +19,10 @@ contacts.each do |name, data|
     data.each do |attribute, value|
       if attribute == :favorite_icecream_flavors
          value.delete_if {|flavor| flavor=="strawberry"}
-      end
+  end
       end  
     end
-  end
+end
     
   #remember to return your newly altered contacts hash!
   contacts
