a6b283f32c65fe552f4f8f7443c39bb9193d4cd8
Tracy Ngo <tngo1206@gmail.com>
2019-02-15 04:07:14 +0000

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,9 +14,16 @@ def first_challenge
   }
 
   #your code here
-
-
+contacts.each do |name, data|
+  if name == "Freddy Mercury"
+    data.each do |attribute, value|
+      if attribute == :favorite_icecream_flavors
+         value.delete_if {|flavor| flavor=="strawberry"}
+  end
+      end  
+    end
+end
+    
   #remember to return your newly altered contacts hash!
   contacts
 end
-
